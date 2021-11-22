1172e35bed45f91401357581cf5d357ea7dec415
Diego Peña Y Lillo <80992641+Diegopyl1209@users.noreply.github.com>
2021-11-22 14:42:16 +0000

    Update test.py

--- Test/test.py
+++ Test/test.py
@@ -30,12 +30,12 @@ class Test(commands.Cog):
 
 
 
-@tasks.loop(seconds=10)
-async def check_list(list1):
-    list2 = requests.get("https://jsonplaceholder.typicode.com/users")
-    if list1 == list2:
-        channel = bot.get_channel(901904896507392061)
-        await channel.send("test")
+    @tasks.loop(seconds=10)
+    async def check_list(list1):
+        list2 = requests.get("https://jsonplaceholder.typicode.com/users")
+        if list1 == list2:
+            channel = bot.get_channel(901904896507392061)
+            await channel.send("test")
 
 
 
