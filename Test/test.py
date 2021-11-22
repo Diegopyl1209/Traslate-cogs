bc71f5f501c5c5e604d66f76c76b43001e954cbf
Diego Peña Y Lillo <80992641+Diegopyl1209@users.noreply.github.com>
2021-11-22 19:17:10 +0000

    Update test.py

--- Test/test.py
+++ Test/test.py
@@ -37,7 +37,7 @@ class Test(commands.Cog):
         channel = self.bot.get_channel(901904896507392061)
 
 
-        json_data = eval(r.text)
+        json_data = eval(r.text).split("\n•")
 
         for i in json_data["games"]:
             await channel.send(i)
