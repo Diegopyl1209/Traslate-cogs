3ee9c19868d211458b18b2f0142c3ed2d78db01f
Diego Peña Y Lillo <80992641+Diegopyl1209@users.noreply.github.com>
2021-11-22 17:52:02 +0000

    Update test.py

--- Test/test.py
+++ Test/test.py
@@ -40,7 +40,7 @@ class Test(commands.Cog):
         package_json = r.json()
         channel = self.bot.get_channel(901904896507392061)
 
-        list2 = json.loads(package_json)
+        #list2 = json.loads(package_json)
         for a in package_json:
             if a in list1:
                 await channel.send(f"tt{a}")
