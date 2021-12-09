0a3412a4e8a35b2156686840d683664eba45e4db
Diego Peña Y Lillo <80992641+Diegopyl1209@users.noreply.github.com>
2021-12-09 21:43:40 +0000

    Update gfn.py

--- gfn/gfn.py
+++ gfn/gfn.py
@@ -34,7 +34,7 @@ class Gfn(commands.Cog):
         channel_id = 743921141864988743
         channel = self.bot.get_channel(channel_id)  
 
-        json_data = await r.json()
+        json_data = r.json()
         embed=discord.Embed(title="Añadidos Geforce Now", description="", color=discord.Color.green())
         embed.set_thumbnail(url="https://www.apkmirror.com/wp-content/uploads/2020/09/36/5f626fb02b86b.png")
         verify = False
